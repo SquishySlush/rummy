4146ad5c1d7509c1c97b7c2d0e3f397b4979d954
Faisal Mustafa <faisalmustafa041008@gmail.com>
2026-02-18 21:46:30 +0400

    validator improvements for wild cards

--- game_logic/validator.py
+++ game_logic/validator.py
@@ -15,27 +15,38 @@ class validator:
             return False
         ranks = []
         for card in cards:
-            ranks.append(card.rank)
+            if card.is_wild != 1:
+                ranks.append(card.rank)
             if len(ranks) != len(set(ranks)):
                 return False
             else:
                 suits = []
                 for card in cards:
-                    suits.append(card.suit.name)
+                    if card.is_wild != 1:    
+                        suits.append(card.suit.name)
                     if len(suits) == len(set(suits)):
                         return True
-                    else:
-                        return False
+                else:
+                    return False
     
     def validate_run(cards, ruleset):
-        
+
         cards = sort_rank(cards)
         card1_index = cards[0].index
         for card in cards:
-            if card.rank != cards[0].rank:
+            if card.rank != cards[0].rank and card.is_wild != 1:
                 return False
             for i in range(len(cards)):
-                if card1_index + i != cards[i].index:
+                if card1_index + i != cards[i].index and cards[i].is_wild != 1:
                     return False      
         return True
-            
+    
+    def validate_meld(self, cards, ruleset):
+        if len(cards) < ruleset.min_meld_size:
+            return False
+        elif self.validate_set(cards, ruleset) or self.validate_run(cards, ruleset):
+            return True
+    
+    def meld_score(cards):
+        
+
