26b1befbd0258541ece189becb73b02594915dae
Faisal Mustafa <faisalmustafa041008@gmail.com>
2026-02-18 09:35:03 +0400

    updated ruleset.py slightly

--- game_logic/ruleset.py
+++ game_logic/ruleset.py
@@ -14,8 +14,25 @@ class Ruleset:
         
         self.min_meld_size = config.get('min_meld_size', 3)
         self.max_meld_size_set = config.get('max_meld_size_set', 4)
-        self.max_meld_size_run = None
+        self.max_meld_size_run = config.get('max_meld_size_run', None)
         
-        self.wilds = config.get('wilds', 'Joker')
-        self.num_wilds = config.get('num_wilds', 0)
+        self.wilds = config.get('wilds', ['Joker'])
+        self.num_wilds = config.get('num_wilds', [0])
         
+        self.scoring_method = config.get('scoring_method', 'negative')
+    
+    def is_wild(self, card):
+        if card.rank in self.wilds:
+            return True
+        else:
+            return False
+    
+    def to_dict(self):
+        return {'allow_sets' : self.allow_sets,
+                'allow_runs' : self.allow_runs,
+                'min_meld_size' : self.min_meld_size,
+                'max_meld_size_set' : self.max_meld_size_set,
+                'max_meld_size_run' : self.max_meld_size_run,
+                'wilds' : self.wilds,
+                'num_wilds' : self.num_wilds,
+                'scoring_method' : self.scoring_method}
