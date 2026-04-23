c089262733b144871cced3715957eb735eed0d71
Faisal Mustafa <faisalmustafa041008@gmail.com>
2026-04-23 23:18:44 +0400

    Forgot to save some stuff

--- Templates/js/MainMenu.js
+++ Templates/js/MainMenu.js
@@ -1,25 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-    await EnsureUserSession();
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     await EnsureUserSession();
+// });
 
-async function EnsureUserSession() {
-    try {
-        const response = await fetch("/auth/guest", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
+// async function EnsureUserSession() {
+//     try {
+//         const response = await fetch("/auth/guest", {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json"
+//             }
+//         });
 
-        const data = await response.json();
+//         const data = await response.json();
 
-        if (!response.ok) {
-            console.error("Guest Initialisation Failed", data);
-            return;
-        }
+//         if (!response.ok) {
+//             console.error("Guest Initialisation Failed", data);
+//             return;
+//         }
 
-        console.log("Session Ready:", data);
-    } catch (error) {
-        console.error("Failed To Create Guest Session:", error);
-    }
-}
+//         console.log("Session Ready:", data);
+//     } catch (error) {
+//         console.error("Failed To Create Guest Session:", error);
+//     }
+// }
